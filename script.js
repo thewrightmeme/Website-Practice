5f0b0a1485ab283d3425ce5df75bb2a3a61b5a32
thewrightmeme <113187784+thewrightmeme@users.noreply.github.com>
2024-03-11 12:01:20 -0400

    js starting

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function subpage() {
+    window.location.href = "Elijah's subpage";
+}
+function differentPage() {
+
+}
+function restyle() {
+    
+}
+
+function changeImage() {
+    
+}
+function changeButtonText() {
+    
+}
