bee60c9e47bf21b4b3603fbf2738f76cbe8e89db
thewrightmeme <113187784+thewrightmeme@users.noreply.github.com>
2024-03-12 12:00:22 -0400

    completed buttons

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ function subpage() {
     window.location.href = "Elijah's subpage";
 }
 function differentPage() {
-
+    window.location.href = "https://www.w3schools.com/";
 }
 function restyle() {
-    
+    document.getElementById("idBody").style.background = "skyblue";
 }
 
 function changeImage() {
-    
+    document.getElementById("img1").src = 'images/backrooms.webp';
 }
 function changeButtonText() {
-    
+    var butt = document.getElementById("button1")
+    butt.value = "value thing";
+    butt.innerHTML = "my text"
 }
