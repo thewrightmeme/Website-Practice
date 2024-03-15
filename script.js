9cd176349f457a811b8c6376753e6f17e2c34e32
thewrightmeme <113187784+thewrightmeme@users.noreply.github.com>
2024-03-15 14:07:20 -0400

    apples

--- script.js
+++ script.js
@@ -13,7 +13,20 @@ function changeImage() {
     document.getElementById("img1").src = 'images/backrooms.webp';
 }
 function changeButtonText() {
-    var butt = document.getElementById("button1")
+    let butt = document.getElementById("button1")
     butt.value = "value thing";
     butt.innerHTML = "my text"
+}
+//_______________________________________________navbar___________________________________________\\
+function redirect1(){
+    window.location.href = "Website-Practice";
+}
+function redirect2(){
+    window.location.href= "subpage 2"
+}
+function redirect3(){
+    window.location.href= "subpage 3"
+}
+function redirect4(){
+    window.location.href= "Elijah's subpage"
 }
